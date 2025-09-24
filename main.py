8e6bd51aaf68f9b23866e575b73153a5f78d23d6
AI researcher <57758974+liam274@users.noreply.github.com>
2025-09-24 13:03:36 +0800

    modified:   main.py

--- main.py
+++ main.py
@@ -247,7 +247,7 @@ while i<script_length:
         while not callable(memory[command]):
             command=memory[command]
             _time+=1
-            if _time>0xFF:
+            if _time>MEM_SIZE:
                 print("Possible infinite loop detected.",file=sys.stderr)
                 sys.exit(1)
         if i+1>=script_length:
